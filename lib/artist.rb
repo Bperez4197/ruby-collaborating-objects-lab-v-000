40b680486966b67f12eb15595176b1852d2e394d
Bperez4197 <bryceperez31@gmail.com>
2017-12-29 03:44:41 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,4 +25,13 @@ class Artist
     @@all
   end
 
+  def self.find_or_create_by_name(name)
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
+      
+
 end
