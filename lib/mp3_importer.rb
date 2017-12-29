8d89a1b098cafe9251d0776dda20ed53af63757e
Bperez4197 <bryceperez31@gmail.com>
2017-12-29 03:56:11 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,10 +8,14 @@ attr_accessor :path
 
   def files
     files = []
-    Dir.new(self.path)each do |file|
+    Dir.new(self.path).each do |file|
       files << file if file.lenth > 4
     end
     files
   end
-  
+
+  def import
+    self.files.each do |filename|
+      Song.new_by_filename(filename)
+    end
 end
